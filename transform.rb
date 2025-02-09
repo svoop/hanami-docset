6d0d70d41ec3520e1c03d96a21a2eb99e74ee344
Sven Schwyn <sven.schwyn@bitcetera.com>
2025-02-09 21:50:23 +0100

    Prefix all pages to make titles like "configuration" unique

--- transform.rb
+++ transform.rb
@@ -16,15 +16,17 @@ main_html = File.read('introduction/getting-started/index.html')
     %r{<nav[> ].*?</nav>}m => ->(*) { '' },
     %r{<ul class="section-nav"[> ].*?</ul>}m => ->(*) { '' },
     %r{>v#{version_re}:\s*}i => ->(*) { '>' },
-    %r{"/v#{version_re}/(.*)"} => ->(*) { '"/../../\1/index.html"' },
-    %r{<h1.*?>Overview</h1>} => ->(prefix) { "<h1>#{prefix} overview</h1>" }
+    %r{"/v#{version_re}/(.*)"} => ->(*) { %("/../../#{$1}/index.html") },
+    %r{<h1.*?>(.*?)</h1>} => ->(*) { "<h1>#{@prefix} – #{$1}</h1>" }
   }
 }.each do |glob, transformations|
   Dir[glob].each do |file|
-    overview_prefix = file.split('/').first.gsub('-', ' ').capitalize
+    @prefix = file.split('/').first.gsub('-', ' ')
+      .capitalize
+      .sub(/(cli|api)/i) { it.upcase }
     File.read(file).then do |html|
-      transformations.each do |pattern, replace_proc|
-        html = html.gsub(pattern, replace_proc.call(overview_prefix))
+      transformations.each do |pattern, replace|
+        html = html.gsub(pattern, &replace)
       end
       File.write(file, html)
     end
